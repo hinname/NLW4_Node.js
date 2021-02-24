6f1b187f2716b269f9fba76474eb021cc1f0e627
Herivelton Costa <herivelton.PRo@gmail.com>
2021-02-24 09:24:07 -0300

    Dia 2 + Inicio dos repositorios

---
+++ api/src/database/migrations/1614169402042-CreateSurveys.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateSurveys1614169402042 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
